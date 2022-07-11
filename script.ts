ce482ab4c3bb84bc13a74b81e428582f5a3d2a51
Gabrielttbr <ttbr2888@gmail.com>
2022-07-11 17:52:15 -0300

    desafio 02

--- script.ts
+++ script.ts
@@ -1,5 +1,29 @@
-let employee  = {
-    code: 15,
-    name: "gabriel"
+interface IPessoa {
+    nome: string,
+    idade: number,
+    profissao: string
 }
-console.log(employee)
+let pessoa1: IPessoa = {
+    nome: "Gabriel",
+    idade: 17,
+    profissao: "Programador"
+}
+let pessoa2: IPessoa = {
+    nome: "Rafael",
+    idade: 17,
+    profissao: "Auxiliar Adiministrativo"
+}
+let pessoa3: IPessoa = {
+    nome: "Felipe",
+    idade: 25,
+    profissao: "Presidente da Santa Rita"
+}
+let pessoa4: IPessoa = {
+    nome: "Francisca",
+    idade: 17,
+    profissao: "Monitoramento"
+}
+console.log(pessoa1);
+console.log(pessoa2);
+console.log(pessoa3);
+console.log(pessoa4);
