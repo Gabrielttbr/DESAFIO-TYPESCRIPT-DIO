eb29efb90617f6bc9fc4088ca323f6d87d5bad83
Gabrielttbr <ttbr2888@gmail.com>
2022-07-11 19:50:31 -0300

    desafio 4

--- script.ts
+++ script.ts
@@ -4,8 +4,8 @@
 // A ideia dessa atividade é criar um aplicativo que:
 //    - Busca filmes
 //    - Apresenta uma lista com os resultados pesquisados
-//    - Permite a criação de listas de filmes e a posterior adição de filmes nela
-
+//    -  Permite a criação de listas de filmes e a posterior adição de filmes nela
+ 
 // Todas as requisições necessárias para as atividades acima já estão prontas, mas a implementação delas ficou pela metade (não vou dar tudo de graça).
 // Atenção para o listener do botão login-button que devolve o sessionID do usuário
 // É necessário fazer um cadastro no https://www.themoviedb.org/ e seguir a documentação do site para entender como gera uma API key https://developers.themoviedb.org/3/getting-started/introduction
